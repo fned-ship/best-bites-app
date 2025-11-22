e8072747c349006ce10263b9aba78ddfbfb6ba95
youssef <fnedyoussef222@gmail.com>
2025-11-22 16:34:50 +0100

    first commit

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  birthDay:Date ;
+  imageSrc:string ;
+  isActive:Boolean ;
+  number:string;
+  address:string;
+}
---
+++ src/app/routes/active/active.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders , HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-active',
+  standalone: true,
+  imports: [CommonModule, RouterModule,HttpClientModule],
+  templateUrl: './active.html',
+  styleUrls: ['./active.css']
+})
+export class Active implements OnInit {
+  serverURL = environment.serverURL; // You can replace with environment variable
+  securitycode: string | null = null;
+
+  errorMsg = '';
+  display = 'none';
+  color = 'hsl(var(--success-hsl))';
+
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.securitycode = this.route.snapshot.paramMap.get('securitycode');
+
+    if (this.securitycode) {
+      const body = { securitycode: this.securitycode };
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+      this.http.post(`${this.serverURL}/activate`, body, { headers, responseType: 'text' })
+        .subscribe({
+          next: (res) => {
+            console.log('Response:', res);
+            if (res == '"verified"' ) {
+              console.log('Response1:', res);
+              this.errorMsg = 'Your account has been successfully verified';
+              this.color = 'hsl(var(--success-hsl))';
+              this.display = 'block';
+            } else {
+              console.log('Response2:', res);
+              this.errorMsg = 'This code is no longer usable';
+              this.color = 'hsl(var(--error-hsl))';
+              this.display = 'none';
+            }
+          },
+          error: (err) => console.error('ERROR:', err)
+        });
+    }
+  }
+}
